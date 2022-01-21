98d57692c7c1822c77f1115f2b0b145612e30575
Rhythm Jayee <jayeerythm8@gmail.com>
2022-01-21 15:02:04 +0530

    end points created

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-const express = require("express")
-const helmet = require("helmet");
-
-
+const express = require("express");
 
 const app = express();
 
-
-app.use(helmet());
-
-app.get("/home", (rep, res) => {
+app.get("/", (rep, res) => {
     res.send("<h1>Hello World!!!</h1>");
 });
 
-app.get("/", (rep, res) => {
+app.get("*", (rep, res) => {
     res.send("<h1>We DONT so THIS here !!! Better to put right URL</h1></h1>");
 });
 
